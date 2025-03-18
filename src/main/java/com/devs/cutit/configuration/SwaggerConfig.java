b843620c30d1c280da387a6cadf12e1b731ff6ae
gsanchezescobar <gsanchezescobar@gmail.com>
2025-03-17 20:47:26 -0500

    descripcion y contacto en swagger

--- src/main/java/com/devs/cutit/configuration/SwaggerConfig.java
+++ src/main/java/com/devs/cutit/configuration/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.devs.cutit.configuration;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,5 +16,21 @@ public class SwaggerConfig {
                 .group("public")
                 .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cutit API")
+                        .version("1.0")
+                        .description("API para la gestión integral de los procesos realizados en el laboratorio de cutit saws.")
+                        .contact(new Contact()
+                                .name("Los Gerentes Dev")
+                                .email("soporte@gerentes.dev")
+                                .url("https://github.com/Gerentes-dev"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
     }
 }
